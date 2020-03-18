b3e68aef083a50924f3fb6ed0a1565398b6510a4
Derek <dvanderl@users.noreply.github.com>
2020-03-18 09:47:25 -0500

    Add project files.

---
+++ EmptySite/Models/Pages/StandardPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EmptySite.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPage", GUID = "6a21a40c-99cf-48ff-96e8-1310ddde4fe9", Description = "")]
+    public class StandardPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "The Page Heading",
+            GroupName = SystemTabNames.Content,
+            Order = 0)]
+        public virtual String Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
